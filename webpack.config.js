d7a9f084da81b9faf7a3003a9ed57290f4c4c18a
Oksana <puga.oksana@gmail.com>
2018-09-27 00:56:33 +0300

    fixed webpack config, added root component

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src');
+var BUILD_DIR = path.resolve(__dirname, 'public');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    entry: APP_DIR + '\\index.jsx',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: '/\.jsx?/',
-                include: APP_DIR,
-                loader: 'babel'
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
             }
         ]
     }
