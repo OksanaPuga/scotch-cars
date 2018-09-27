eb480ce978fb0271edaaf011f39687307f9f3ea8
Oksana <puga.oksana@gmail.com>
2018-09-27 13:43:33 +0300

    added more routes and components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class Home extends Component {
-    render() {
-        return (<h1>Hiii!!!!</h1>);
-    }
-}
+import Home from './common/home.jsx';
+import About from './common/about.jsx';
+import Car from './car/car.jsx';
 
-render(<Home />, document.getElementById('container'));
+
+render(
+    <BrowserRouter>
+        <div>
+            <Route path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/car' component={Car} />
+        </div>
+    </BrowserRouter>,
+    document.getElementById('container')
+);
