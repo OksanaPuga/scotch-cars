725ff2584be4d2231fe04cd31e603446cac2f9a6
Oksana <puga.oksana@gmail.com>
2018-09-28 22:42:41 +0300

    added some mocked data

--- src/car/car.jsx
+++ src/car/car.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import carsData from '../../mocks.js';
 
 class Car extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ cars: carsData });
+    }
+
     render() {
-        return (<h1>Cars Page</h1>);
+        let cars = this.state.cars.map((car) => (
+            <a
+                className='list-group-item'
+                href='#'
+                key={car.id}>
+                {car.name}
+            </a>
+        ));
+
+        return (
+            <div>
+                <h1>Cars Page</h1>
+                <div className='list-group'>
+                    {cars}
+                </div>
+            </div>
+        );
     }
 }
 
